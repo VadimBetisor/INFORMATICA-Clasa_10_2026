b293522db96248ab7903215888b592d61d9c3d56
Bețișor Vadim <68860557+VadimBetisor@users.noreply.github.com>
2026-09-15 10:49:57 +0300

    Update Problema 1 BVN 15-09.py

--- Problema 1 BVN 15-09.py
+++ Problema 1 BVN 15-09.py
@@ -1,2 +1,4 @@
 print('Struguri','100','kg',sep='    ',end='\n')
 print('Mere','10','t',sep='        ',end='\n')
+print('Struguri','100','kg',sep='    ',end='\n')
+print('Mere','10','t',sep='        ',end='\n')
